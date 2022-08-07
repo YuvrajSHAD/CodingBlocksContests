c5add14435df186dbf783701ffcd064d34877517
Yuvraj Singh <71546888+YuvrajSHAD@users.noreply.github.com>
2022-08-07 15:43:54 +0530

    Create LowerUpper.cpp

---
+++ Contest-1/LowerUpper.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	char ch;
+	cin >> ch;
+
+	if(ch >= 'a' && ch <='z')cout << "lowercase";
+	else if( ch >= 'A' && ch <= 'Z')cout << "UPPERCASE";
+	else cout << "Invalid";
+	return 0;
+}
